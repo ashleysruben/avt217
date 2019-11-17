4d9b2dce122fe1cb9d65a19df3f68f711cc827ce
Ashley <aruben@gmu.edu>
2019-11-17 09:45:14 -0500

    Update main.js

--- e17/js/main.js
+++ e17/js/main.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
   console.log("ready");
 
   $('.one').click(function(){
-    $('.lab').toggleClass('align-left')
-    });
+    $('.lab').toggleClass('align-left');
+  });
 
   $('.two').click(function(){
-    $('.corgi').toggleClass('align-right')
+    $('.corgi').toggleClass('align-right');
   });
 
   $('.three').click(function(){
-      $('.beags').toggleClass('up')
+      $('.beags').toggleClass('up');
   });
 });
