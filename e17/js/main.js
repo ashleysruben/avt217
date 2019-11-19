019a3438795cc41096d034bdc41bb52c32c65a8e
Ashley <aruben@gmu.edu>
2019-11-19 08:36:28 -0500

    b

--- e17/js/main.js
+++ e17/js/main.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 
   $('.one').click(function(){
     $('.lab').toggleClass('align-left');
-  });
+  })
 
   $('.two').click(function(){
     $('.corgi').toggleClass('align-right');
   });
 
   $('.three').click(function(){
-      $('.beags').toggleClass('up');
+      $('.beags').toggleClass('uppercase');
   });
 });
