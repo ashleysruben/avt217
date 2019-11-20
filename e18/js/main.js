17b524fabb4a9d8d7df40f63e5672dc346c0e919
Ashley <aruben@gmu.edu>
2019-11-20 06:57:43 -0500

    e18

--- e18/js/main.js
+++ e18/js/main.js
@@ -1,26 +1,37 @@
 $(document).ready(function() {
-   var number = Math.floor(Math.random() * 25);
-   console.log(number);
-   var guesses = '';
-   $('#enter').click(function() {
-     var guess = $('input').val();
-     guesses += digit + ' ';
-     $('.guesses p').text(guesses);
-     console.log(digit);
-     if (number == digit) {
-       $('h1').text(number + 'Correct!');
-       $('input').val('');
-     } else if (number < digit) {
-       $('h1').text(digit + 'Too high');
-       $('input').val('')
-     } else {
-       $('h1').text(digit + 'Too low');
-       $('input').val('');
-     }
-   });
-   $(document).keypress(function(e){
-       if(e.which == 9){ //Enter key pressed
-           $('#submit').click();//Trigger button click event
-       }
-   });
+    // run function on initial page load
+	animalGuess();
 });
+
+function animalGuess() {
+	var animalArray = ["Dog","Cat","Kangaroo","Skunk","Hippopotamus","Jackrabbit","Fox","Manatee","Swordfish","Crab","Alligator","Cow","Octopus","Turtle","Pig","Sloth","Tarantula","Iguana","Starfish","Possum","Sparrow","Grizzly Bear","Zebra","Goat","Penguin","Ostrich","Alpaca","Goose","Porcupine","Monkey","Bumblebee","Monarch Butterfly","Praying Mantis","Racoon","Polar Bear"];
+	var randomNumber = Math.floor(Math.random() * animalArray.length);
+	var stripped = animalArray[randomNumber].toLowerCase();
+	console.log(animalArray[randomNumber]);
+	console.log(stripped);
+	$('#submit').click(function() {
+		var input = $('input').val();
+		var guess = input.toLowerCase();
+		console.log(guess);
+		if (guess == stripped || guess == animalArray[randomNumber]) {
+			$('h1').text('Correct!');
+			$('input').val('');
+			$('.button p').text('Play Again!');
+			$('#submit').click(function() {
+				location.reload(true);
+			});
+		} else  {
+			$('h1').text('Incorrect!');
+			$('input').val('');
+			$('.button p').text('Play Again?');
+			$('#submit').click(function() {
+				location.reload(true);
+			});
+		}
+	});
+	$(document).keypress(function(e){
+			if(e.which == 13){ //Enter key pressed
+					$('#submit').click();//Trigger button click event
+			}
+	});
+}
